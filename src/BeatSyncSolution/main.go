02def8e8916fb4469a370a60e571e5c58330936e
mileskj <mileskjordan@gmail.com>
2024-08-27 11:29:27 -0400

    Adding functions to zotify file, writing comments on authentication issue, must fix import issue

--- src/BeatSyncSolution/main.go
+++ src/BeatSyncSolution/main.go
@@ -4,19 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"os/exec"
+	spotify
 )
-
-func runCommand(command []string) {
-	cmd := exec.Command(command[0], command[1:]...)
-	stdout, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Println(string(stdout))
-}
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>BeatSyncSolution</h1>")
@@ -25,7 +15,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	fmt.Println("Starting web server on :3000")
-	os.Setenv("PATH", os.Getenv("PATH")+":/root/.local/bin")
+
 	runCommand([]string{"sh", "-c", "zotify --help"})
 
 	http.ListenAndServe(":3000", nil)
--- src/BeatSyncSolution/spotify/zotify.go
+++ src/BeatSyncSolution/spotify/zotify.go
@@ -1,1 +1,34 @@
 package spotify
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+var zotifyPath = "/root/.local/bin"
+
+func runCommand(command []string) {
+	cmd := exec.Command(command[0], command[1:]...)
+	stdout, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println(string(stdout))
+	//Don't need to worry about saving the stdout until I find that I do need to worry about it
+	//Print the Stdout of this, but should I also be tracking the stderr?
+}
+
+func setupZotify() {
+	//Adds Zotify to the PATH so we can call it normally
+	os.Setenv("PATH", (os.Getenv("PATH") + ":" + zotifyPath))
+
+	//Need to figure out authentication
+	//Authentication has changed because of the Spotify API, now need to run a script to get a proper credentials.json here: https://github.com/dspearson/librespot-auth.git
+	//Rust toolchain had to be installed to then run this script, to then run a build on the script from the repo above
+	//Maybe need to include this stuff in the Dockerfile on setup
+	//If this is a librespot problem, then this will happen regardless on if I use Zotify or not
+	//It seems to specifically get a complicated auth token for your account, maybe theres a way to do this automatically?
+}
